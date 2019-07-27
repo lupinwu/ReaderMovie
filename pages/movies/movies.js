89cc163ef5845632151390a5de29d752d227d6d9
lupin <mailwp@163.com>
2019-07-27 23:25:47 +0800

    movie list 7-13

--- pages/movies/movies.js
+++ pages/movies/movies.js
@@ -13,17 +13,18 @@ Page({
    * 生命周期函数--监听页面加载
    */
   onLoad: function(options) {
-    var inTheatersUrl = app.globalData.doubanBase + '/v2/movie/in_theaters';
-    var comingSoonUrl = app.globalData.doubanBase + '/v2/movie/coming_soon';
-    var top250Url = app.globalData.doubanBase + '/v2/movie/top250';
+    var inTheatersUrl = app.globalData.doubanBase + '/v2/movie/in_theaters' + '?start=0&count=3';
+    var comingSoonUrl = app.globalData.doubanBase + '/v2/movie/coming_soon' + '?start=0&count=3';
+    var top250Url = app.globalData.doubanBase + '/v2/movie/top250' + '?start=0&count=3';
 
     this.getMovieListData(inTheatersUrl);
-    this.getMovieListData(comingSoonUrl);
-    this.getMovieListData(top250Url);
+    // this.getMovieListData(comingSoonUrl);
+    // this.getMovieListData(top250Url);
   },
 
 
   getMovieListData: function(url) {
+    var that = this;
     wx.request({
       url: url,
       method: 'GET',
@@ -32,11 +33,32 @@ Page({
       },
       success: function(res) {
         console.log(res);
-        console.log(url);
+        that.processDoubanData(res.data)
       },
       fail: function(res) {
         console.log('failed');
       }
+    })
+  },
+
+  processDoubanData: function(moviesDouban) {
+    var movies = [];
+    for (var idx in moviesDouban.subjects) {
+      var subject = moviesDouban.subjects[idx];
+      var title = subject.title;
+      if (title.length >= 6) {
+        title = title.substring(0, 6) + '...';
+      }
+      var temp = {
+        title: title,
+        average: subject.rating.average,
+        coverageUrl: subject.images.large,
+        movieId: subject.id
+      }
+      movies.push(temp)
+    }
+    this.setData({
+      movies: movies
     })
   }
 })
